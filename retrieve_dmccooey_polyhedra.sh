a93e25ddd3ef6b3b9eec7b681db99358cb2aa5ed
Leonardo Javier Belen <leobelen@gmail.com>
2018-10-01 19:55:24 -0300

    RpolyhedraDB release v0.2.7

--- retrieve_dmccooey_polyhedra.sh
+++ retrieve_dmccooey_polyhedra.sh
@@ -1,4 +1,5 @@
 #!/usr/local/bin/bash
+cd sources
 wget -r -np -k http://dmccooey.com/polyhedra
 
 for filename in `find dmccooey.com/polyhedra/*.html -type f`; do
@@ -10,4 +11,5 @@ for filename in `find dmccooey.com/polyhedra/*.js -type f`; do
     rm $filename
 done
 
+cd ..
 echo "The txt were successfully downloaded"
--- retrieve_netlib_polyedra.sh
+++ retrieve_netlib_polyedra.sh
@@ -1,2 +1,4 @@
 #!/usr/local/bin/bash
+cd sources
 wget -r -np -k http://www.netlib.org/polyhedra/
+cd ..
